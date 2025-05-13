1c894b68f0c411f273ab7c303814765e38075534
cat2151 <fct_2004@mail.goo.ne.jp>
2025-05-13 22:02:23 +0900

    指定したゲームをプレイ中は、コマンドチャレンジのウィンドウを最前面表示しない（最背面にして指定秒待つ）ようにした。tomlで指定する。

--- command-challenge.bat
+++ command-challenge.bat
@@ -1,1 +1,2 @@
-python main.py
+python main.py --config-filename command_challenge.toml
+if not %errorlevel% == 0 (pause & exit /b 1)
--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import pygame
 from controller import detect_controller_input
+from get_window_info import get_active_window_process_name
 from gui import do_backmost, do_topmost, init_tkinter
+from utils import get_args, update_args_by_toml
 
 def main():
+    args = get_args()
+    args = update_args_by_toml(args, args.config_filename)
+
     joystick = init_joystick()
     (root, label) = init_tkinter()
-    main_loop(root, label, joystick)
+    main_loop(root, label, joystick, args)
 
 def init_joystick():
     if hasattr(pygame, 'init'):
@@ -20,16 +25,36 @@ def init_joystick():
         print(f"{i}: {joystick.get_name()} - ボタン数: {joystick.get_numbuttons()}")
     return joystick
 
-def main_loop(root, label, _joystick):
+def main_loop(root, label, _joystick, args):
     timer_id_dict = {"id": None}
     clock = pygame.time.Clock()
+    check_interval_msec = int(args.backmost_mode["check_interval_sec"] * 1000)
+    last_check_msec = pygame.time.get_ticks()
+
     while True:
+        check_and_do_backmost(root, args, check_interval_msec, last_check_msec)
+
         result = detect_controller_input()
         if result:
             show_input(root, label, result, timer_id_dict)
         root.update_idletasks()
         root.update()
         clock.tick(60)
+
+def check_and_do_backmost(root, args, check_interval_msec, last_check_msec):
+    current_msec = pygame.time.get_ticks()
+    if current_msec - last_check_msec >= check_interval_msec:
+        if is_playing_game(args):
+            do_backmost_and_wait(root, args)
+        last_check_msec = current_msec
+
+def is_playing_game(args):
+    return get_active_window_process_name() == args.backmost_mode["process_name"]
+
+def do_backmost_and_wait(root, args):
+    do_backmost(root)
+    while is_playing_game(args):
+        pygame.time.wait(10000)
 
 def show_input(root, label, text, timer):
     def reset_topmost():
