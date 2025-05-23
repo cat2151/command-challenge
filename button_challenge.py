1ff84bc5eca2668e4591996d6dfe59d3beb8ecc0
cat2151 <fct_2004@mail.goo.ne.jp>
2025-05-23 23:15:59 +0900

    GUI windowを表示するようにした

--- button_challenge.py
+++ button_challenge.py
@@ -1,3 +1,4 @@
+import tkinter
 import pygame
 from button_utils import load_all_configs
 from button_input import create_button_states, get_buttons_as_bitstring, setup_pygame_and_joystick, shutdown_pygame
@@ -15,6 +16,8 @@ def main():
     if joystick is None:
         return
 
+    (tkinter_root, _label) = init_tkinter()
+
     try:
         while True:
             pygame.event.pump()
@@ -24,11 +27,23 @@ def main():
             (mission, mission_result, mission_index) = check_and_update_mission(plus, missions, mission_index, lever_plus_pressed)
             update_display(names, mission, buttons_bits, lever_plus_pressed, mission_result)
 
+            tkinter_root.update_idletasks()
+            tkinter_root.update()
+
             pygame.time.wait(100) # 16だと点滅が激しくて見づらかった
     except KeyboardInterrupt:
         print("プログラムを終了します。")
     finally:
         shutdown_pygame()
+
+def init_tkinter():
+    root = tkinter.Tk()
+    # TODO 各パラメータをtomlで指定可能にする
+    root.title("ボタンチャレンジ 終了はterminalでCTRL+C（予定）")
+    root.geometry("640x80")
+    label = tkinter.Label(root, text="", font=("Arial", 20))
+    label.pack()
+    return root, label
 
 if __name__ == "__main__":
     main()
